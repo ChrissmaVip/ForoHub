8c16f2cfe77fb8367b1567215bed0629595ab2de
Christian Galda <126181953+ChrissmaVip@users.noreply.github.com>
2024-07-17 00:24:45 -0300

    Add files via upload

--- Foro/src/main/java/com/ForoHub/Foro/controller/TopicoController.java
+++ Foro/src/main/java/com/ForoHub/Foro/controller/TopicoController.java
@@ -7,11 +7,7 @@ import com.ForoHub.Foro.Topico.Topico;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -30,12 +26,22 @@ public class TopicoController {
     //guardar en base de datos
         repositoryTopico.save(topico);
     }
-
+//Listar los topicos
     @GetMapping
     public List<DtoTopicoParaMostrar> listarTopicos() {
 
         return repositoryTopico.findAll().stream()
                 .map(DtoTopicoParaMostrar :: new).toList();
+    }
+
+    //borrar un registro Delete
+    @DeleteMapping("/{id}")
+    @Transactional
+    public void deleteTopico(@PathVariable Long id){
+
+    Topico topico = repositoryTopico.getReferenceById(id);
+    repositoryTopico.delete(topico);
+
     }
 
 
